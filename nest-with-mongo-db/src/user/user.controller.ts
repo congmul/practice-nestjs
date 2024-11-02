90ef38f9c74a03fd7ba37920690edca83191f633
Jehyun Jung <congmul@hotmail.com>
2024-11-02 07:31:50 -0700

    create user dto (data transfer object)

--- nest-with-mongo-db/src/user/user.controller.ts
+++ nest-with-mongo-db/src/user/user.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 // {baseURL}/user
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
-    
+
     @Post()
-    async create(@Body() createUserDto: { name: string; email: string; age: number }): Promise<User> {
+    async create(@Body() createUserDto: CreateUserDto): Promise<User> {
         return this.userService.createUser(createUserDto.name, createUserDto.email, createUserDto.age)
     }
 
