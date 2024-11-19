f326ba510fba05a3d50147b0c7c25f889a6344ae
Jehyun Jung <congmul@hotmail.com>
2024-11-19 10:22:43 -0800

    updated

--- nest-with-mongo-db/src/team/team.controller.ts
+++ nest-with-mongo-db/src/team/team.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { TeamService } from './team.service';
 
 @Controller('team')
-export class TeamController {}
+export class TeamController {
+    constructor(private readonly teamService: TeamService){}
+    
+    @Post()
+    async create(){
+        
+    }
+}
